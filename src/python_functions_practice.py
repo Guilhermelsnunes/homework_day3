5f6c24aa5edbdb90d819e25ddc558605f816220c
Tamer <Tamer_amer@outlook.com>
2021-11-10 16:54:04 +0000

    first commit

---
+++ src/python_functions_practice.py
@@ -0,0 +1,25 @@
+def return_10():
+    return 10
+
+def add(number1,number2):
+    return number1+number2
+
+def subtract(number1,number2):
+    return number1-number2
+
+def multiply(number1,number2):
+    return number1*number2
+
+def divide(number1,number2):
+    return number1/number2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string_1,string_2):
+    return string_1 + string_2 
+
+def add_string_as_number(string_1,string_2):
+    return int(string_1)+int(string_2)
+
+def number_to_full_month_name(number):
